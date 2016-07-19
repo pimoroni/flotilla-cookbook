917e1535ac2918a89e55f7470b4533061ea77795
Phil Howard <phil@gadgetoid.com>
2016-07-19 17:11:33 +0100

    Added HSV to rainbow API

--- cookbook.js
+++ cookbook.js
@@ -152,6 +152,27 @@ cookbook.wrappers = {
 
         }
 
+        this.set_pixel_hsv = function(index, h, s, v){
+
+            var r, g, b, i, f, p, q, t;
+            i = Math.floor(h * 6);
+            f = h * 6 - i;
+            p = v * (1 - s);
+            q = v * (1 - f * s);
+            t = v * (1 - (1 - f) * s);
+            switch (i % 6) {
+                case 0: r = v, g = t, b = p; break;
+                case 1: r = q, g = v, b = p; break;
+                case 2: r = p, g = v, b = t; break;
+                case 3: r = p, g = q, b = v; break;
+                case 4: r = t, g = p, b = v; break;
+                case 5: r = v, g = p, b = q; break;
+            }
+
+            this.set_pixel(index, Math.round(r * 255), Math.round(g * 255), Math.round(b * 255));
+
+        }
+
         this.set_pixel = function(i, r, g, b){
 
             if(i >= 0 && i < 5){
